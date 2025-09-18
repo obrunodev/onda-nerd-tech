e316b9b490d3e8eaffae605565da4d0d18d57d25
Bruno R. Pianca <brunorpdev@gmail.com>
2025-09-18 12:03:46 -0300

    feat: Implementa Bootstrap 5.3 em telas financeiras

--- apps/finance/forms.py
+++ apps/finance/forms.py
@@ -8,3 +8,8 @@ class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ['title', 'value', 'due_date']
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "value": forms.NumberInput(attrs={"class": "form-control"}),
+            "due_date": forms.DateInput(attrs={"class": "form-control", "type": "date"}),
+        }
--- apps/finance/views.py
+++ apps/finance/views.py
@@ -1,27 +1,28 @@
 from apps.finance.models import Transaction
 from apps.finance.forms import TransactionForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
-class TransactionCreateView(CreateView):
+class TransactionCreateView(LoginRequiredMixin, CreateView):
     model = Transaction
     form_class = TransactionForm
     success_url = reverse_lazy('finance:list')
 
 
-class TransactionListView(ListView):
+class TransactionListView(LoginRequiredMixin, ListView):
     model = Transaction
     context_object_name = 'transactions'
 
 
-class TransactionUpdateView(UpdateView):
+class TransactionUpdateView(LoginRequiredMixin, UpdateView):
     model = Transaction
     form_class = TransactionForm
     success_url = reverse_lazy('finance:list')
 
 
-class TransactionDeleteView(DeleteView):
+class TransactionDeleteView(LoginRequiredMixin, DeleteView):
     model = Transaction
     success_url = reverse_lazy('finance:list')
